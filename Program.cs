6b4232228eacc32b3872211a9da13004c8f4dd7d
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-23 01:25:04 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -11,4 +11,9 @@ int age = Convert.ToInt32(ReadLine());
 
 int keschasAge = 4;
 int ageDiffernce = age - keschasAge;
-WriteLine("Kescha's age: {0}", ageDiffernce);
+WriteLine("Sizning yoshingiz Kesshani yoshidan shunchaga farq qiladi: {0}", ageDiffernce);
+
+WriteLine($"Siz keshadan kattasiz: {ageDiffernce > 0}");
+WriteLine($"Kessha sizdan katta: {keschasAge > age}");
+WriteLine($"Kessha bilan yoshingiz teng: {keschasAge == age}");
+WriteLine($"Kesshani yoshi siz bilan bir xil emas: {keschasAge != age}");
