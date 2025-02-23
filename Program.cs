f50d37ec9e8b34d9033329ef2ddf6f77ad77aa8c
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-23 20:50:30 +0500

    vazifa bajarildi

--- Program.cs
+++ Program.cs
@@ -1,69 +1,86 @@
 ﻿using System;
 using static System.Console;
 
-// 1. Doira yuzi va aylana uzunligini hisoblash
-
-// Console dan raduisni qiymatini oling va erkanga doiraning yuzi (S=pi*radius^2) va 
-// aylana uzunligini (L=2*pi*radius) erkanga chiqaring.
-// Input: radius=3
-// Output: S=28.2743338823081, L=18.8495559215388
-// --------------
-// Input: radius=4.23
-// Output: S=56.2122031914168, L=26.5778738493697
+// 1. Kalkulyator:
+// Foydalanuvchidan ikkita raqam va operatsiyani (+, -, *, /) so'raydigan kalkulyator dasturini yozing va keyin operatsiya natijasini chop eting.
+// Input = 5;
+// Input = +;
+// Input = 5;
+// Output = 10;
 
 WriteLine("====== 1-Masala ======");
-Write("Radiusni kiriting: ");
-double radius = Convert.ToDouble(ReadLine());
+Write("Enter the first number: ");
+double firstNumber = Convert.ToDouble(ReadLine());
 
-// double pi = 3.14;
-double pi = Math.PI;
-double S = pi * (radius * radius); // S = pi * rad^2 
-double L = 2 * pi * radius; // L = 2pr
-WriteLine($"Yuzi: {S} , aylana uzunligi {L}");
+Write("Iltimos ishorani tanlang: +, -, *, / : ");
+char ishora = Convert.ToChar(ReadLine());
+
+Write("Enter the second number: ");
+double secondNumber = Convert.ToDouble(ReadLine());
+
+double natija = 0;
+
+switch (ishora)
+{
+    case '+':
+        natija = firstNumber + secondNumber;
+        break;
+    case '-':
+        natija = firstNumber - secondNumber;
+        break;
+    case '*':
+        natija = firstNumber * secondNumber;
+        break;
+    case '/':
+        natija = firstNumber / secondNumber;
+        break;
+}
+
+WriteLine($"Answer: {natija}");
 
 
 
 
-// 2. Valyuta konvertri
-
-// Bir valyutadagi summani sumga aylantiruvchi dastur tuzing. Summa va valyuta kursini 
-// e’lon qiling va konvertatsiya qilingan summani hisoblang. Natijani ekranga chiqaring.
-// Input: qiymat=2, kurs=12400 so’m
-// Output: 24800 so’m
-// ----------------
-// Input: qiymat=7.6, kurs=12400 so’m
-// Output: 94240 so’m
+// 2. Raqamlar qatorining yig‘indisini hisoblash:
+// Foydalanuvchidan N musbat butun sonni so‘ragan, so‘ngra 1 dan N gacha bo‘lgan barcha sonlar yig‘indisini hisoblab, natijani ekranga chiqaradigan dastur tuzing.
+// Input = 10;
+// Output = 55;
 
 WriteLine("\n====== 2-Masala ======");
+Write("N musbat sonini kiriting: ");
+int N = Convert.ToInt32(ReadLine());
 
-Write("Miqdorni kiriting: ");
-double qiymay =  Convert.ToDouble(ReadLine());
+if (N < 1)
+{
+    WriteLine("N soni musbat bo'lishi kerak.");
+    return;
+}
 
-WriteLine("Valyuta kursini kiriting: ");
-double kurs = Convert.ToDouble(ReadLine());
+int yigindi = 0;
+for (int i = 1; i <= N; i++)
+{
+    yigindi += i;
+}
 
-double sum = qiymay * kurs;
-
-WriteLine($"Konvertatsiya qilingan summa: {sum} so'm");
+WriteLine($"1 dan {N} gacha bo‘lgan sonlar yig‘indisi: {yigindi}");
 
 
-// 3. Yoshni hisoblash
 
-// Foydalanuvchining tug’ilgan yilini (int x) consoledan oling. Uning yoshini kunlarda 
-// ifodalang. Kabisa yilini hisobga olmang
-// Input: x=2004
-// Output: 6935  ======== bu javob 2023 yil miqiyosida qaytar ekan
-// -----------------
-// Input: x= 1996
-// Output: 9855 ======== bu javob 2023 yil miqiyosida qaytar ekan
+
+// 3.  Paritet tekshiruvi:
+// Foydalanuvchi kiritgan raqamning juft yoki toq ekanligini aniqlaydigan va shunga mos ravishda xabarni chiqaradigan dastur tuzing.
+// Input = 3;
+// Output = “Toq”
 
 WriteLine("\n====== 3-Masala ======");
-Write("Tug'ilgan yilingizni kiriting: ");
-int x = Convert.ToInt32(ReadLine());
+Write("Sonni kiriting: ");
+int son = Convert.ToInt32(ReadLine());
 
-Write("Xozirgi yilni kiriting: ");
-int currentlyYear = Convert.ToInt32(ReadLine());
-int ageInYears = currentlyYear - x;
-int ageInDays = ageInYears * 365;
-
-WriteLine($"Siz taxminan {ageInDays} kun yashagansiz.");
+if (son % 2 == 0)
+{
+    WriteLine("Juft");
+}
+else
+{
+    WriteLine("Toq");
+}
