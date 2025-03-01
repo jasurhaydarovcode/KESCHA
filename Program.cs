841f9c4484083db7c53a77ac9423e43c9d38d215
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-03-01 20:17:48 +0500

    Vazifa bajarildi

--- Program.cs
+++ Program.cs
@@ -1,20 +1,107 @@
-﻿using System;
+﻿// ﻿ Basics.Tanlash bo’limi uchun 1-amaliy vazifa
+using System;
 using static System.Console;
 
-Write("Enter your name: ");
-string name = ReadLine();
-string greating = $"Hello {name}";
-WriteLine(greating);
+// 1. String ustida amallar
+// String tipida (string str) qiymat berilgan. Consoleda int tipidagi qiymat kiriting. Agar bu son str ning uzunligidan katta bo’lsa, str ni katta harflarga o’giring, aks holda kichik harflarga o’giring.
+// Input: x=13, str=”teLefOn”
+// Output: “TELEFON”
+// ---------------
+// Input: x=4, str=”komPyuter”
+// Output: “kompyuter”
 
-Write("Enter your age: ");
-int age = Convert.ToInt32(ReadLine());
 
-int keschasAge = 4;
-int ageDiffernce = age - keschasAge;
-WriteLine("Sizning yoshingiz Kesshani yoshidan {0} ga farq qiladi.", ageDiffernce);
+WriteLine("====== 1-Masala ======");
 
-WriteLine($"Kesha sizdan katta: {keschasAge > age}");
-WriteLine($"Siz keshadan kattasiz: {keschasAge < age}");
-WriteLine($"Siz Kessha bilan tengsiz: {keschasAge == age}");
-WriteLine($"Siz Kesshadan kattasiz yoki tengsiz: {keschasAge <= age}");
-WriteLine($"Kessha siz bilan teng emas: {keschasAge != age}");
+string str = "komPyuter";
+WriteLine("Sonni kiriting: ");
+int son = Convert.ToInt32(ReadLine());
+
+// if, else operation
+// if(son > str.Length){
+//     WriteLine(str.ToUpper());
+// }
+// else{
+//     WriteLine(str.ToLower());
+// }
+
+// Ternariy Operation
+string result =
+    son > str.Length
+        ? str.ToUpper()
+        : str.ToLower();
+
+WriteLine(result);
+
+
+
+
+
+
+
+
+
+// 2. Quyida ternary operator bilan ifodalandan qiymatni if-else yordamida ifodalang.
+
+WriteLine("\n====== 2-Masala ======");
+
+WriteLine("Ternary operatorida yozilgan kod if else yordamida ifodalandi!");
+int x = 15;
+int y = 10;
+
+// string result = (x > y)
+//     ? "x is grater than y"
+//     : (x < y)
+//         ? "x is less than y"
+//         : (x == y)
+//             ? "x is equal to y"
+//             : "x and y are not comparable";
+
+// WriteLine(result);
+
+if (x > y)
+{
+    WriteLine("x is greater than y");
+}
+else if (x < y)
+{
+    WriteLine("x is less than y");
+}
+else if (x == y)
+{
+    WriteLine("x is equal to y");
+}
+else
+{
+    WriteLine("x and y are not comparable");
+}
+
+
+
+
+
+
+
+
+
+// 3. Consoleda o’zbek tilida kiritilgan hafta kunini ingliz tiliga o’girib,
+//  natijani ekranga chiqaradigan dastur tuzing. Switch expression dan foydalaning!
+
+
+WriteLine("\n====== 3-Masala ======");
+
+WriteLine("Insatlgan hafta kunini kiriting (o'zbek tilida): ");
+string operation = ReadLine();
+string natija = operation switch
+{
+    "dushanba" => "Monday",
+    "seshanba" => "Tuesday",
+    "chorshanba" => "Wednesday",
+    "payshanba" => "Thursday",
+    "juma" => "Friday",
+    "shanba" => "Saturday",
+    "yakshanba" => "Sunday",
+    _ => "kichik hafrlar bilan kiriting ||  No'tog'ri hafta kuni!"
+};
+
+WriteLine(natija);
